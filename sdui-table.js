bc64dd2d266bcbb3d4f1ab3e88c51b7d22b26e27
JanMP <pille@mac.com>
2021-06-29 12:10:37 +0200

    handover

--- sdui-table.js
+++ sdui-table.js
@@ -3,3 +3,7 @@
 // Variables exported by this module can be imported by other packages and
 // applications. See sdui-table-tests.js for an example of importing.
 export const name = 'sdui-table';
+// import queryUiObjectToQuery from './imports/ui/QueryEditor/queryUiObjectToQuery'
+// export { currentUserIsInRole, useCurrentUserIsInRole, currentUserMustBeInRole } from './imports/helpers/roleChecks';
+// export { default as createAutoDataTableBackend } from './imports/api/createAutoDataTableBackend.coffee';
+// export * from './imports/helpers/simpleSchemaExtension.coffee.md';
