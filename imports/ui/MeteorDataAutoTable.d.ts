9eb3a3d8c13f600bea4398664196c1deaa034802
JanMP <pille@mac.com>
2021-07-30 11:45:57 +0200

    wrap in declare module

--- imports/ui/MeteorDataAutoTable.d.ts
+++ imports/ui/MeteorDataAutoTable.d.ts
@@ -1,41 +1,43 @@
 /**
  * Some Additional comment
  */
+declare module "meteor/janmp:sdui-table"
+{
+  interface MeteorDataAutoTableProps {
+    sourceName?: string;
+    listSchemaBridge: any;
+    usePubSub: boolean;
+    rowsCollection: any;
+    rowCountCollection: any;
+    query: object;
+    perLoad?: number;
+    canEdit?: boolean;
+    formSchemaBridge: any;
+    canSearch?: boolean;
+    canAdd?: boolean;
+    onAdd?: boolean;
+    canDelete?: boolean;
+    //* @default "Soll der Eintrag wirklich gelöscht werden?"
+    deleteConfirmation?: string;
+    onDelete?: () => void;
+    canExport?: boolean;
+    onExportTable?: () => void;
+    onRowClick?: () => void;
+    autoFormChildren: any;
+    formDisabled?: boolean;
+    formReadOnly?: boolean;
+    useSort?: boolean;
+    getRowMethodName?: () => string;
+    getRowCountMethodName?: () => string;
+    rowPublicationName?: string;
+    rowCountPublicationName?: string;
+    submitMethodName?: string;
+    deleteMethodName?: string;
+    fetchEditorDataMethodName?: string;
+    setValueMethodName?: string;
+    exportRowsMethodName?: string;
+    viewTableRole?, editRole?, exportTableRole?: string;
+  }
 
- declare interface MeteorDataAutoTableProps {
-  sourceName?: string;
-  listSchemaBridge: any
-  usePubSub: boolean;
-  rowsCollection: any;
-  rowCountCollection: any;
-  query: object;
-  perLoad?: number;
-  canEdit?: boolean;
-  formSchemaBridge: any;
-  canSearch?: boolean;
-  canAdd?: boolean; 
-  onAdd?: boolean;
-  canDelete?: boolean;
-  //* @default "Soll der Eintrag wirklich gelöscht werden?"
-  deleteConfirmation?: string;
-  onDelete?: () => void;
-  canExport?: boolean;
-  onExportTable?: () => void; 
-  onRowClick?: () => void;
-  autoFormChildren: any;
-  formDisabled?: boolean;
-  formReadOnly?: boolean;
-  useSort?: boolean;
-  getRowMethodName?: () => string;
-  getRowCountMethodName?: () => string;
-  rowPublicationName?: string;
-  rowCountPublicationName?: string;
-  submitMethodName?: string;
-  deleteMethodName?: string;
-  fetchEditorDataMethodName?: string;
-  setValueMethodName?: string;
-  exportRowsMethodName?: string;
-  viewTableRole?, editRole?, exportTableRole?: string;
+  export default function MeteorDataAutoTable(props: MeteorDataAutoTableProps): JSX.Element;
 }
-
-export default function MeteorDataAutoTable(props: MeteorDataAutoTableProps): JSX.Element;
